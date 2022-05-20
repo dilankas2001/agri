58b3b421bec91c2c01b2543f4353608b35cca9be
dilankas2001 <dilankasandeepa1214@gmail.com>
2022-05-20 10:49:11 +0530

    first

---
+++ android/app/src/main/kotlin/com/example/appagrih/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.appagrih
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const FirstPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class FirstPage extends StatefulWidget {
+  const FirstPage({Key? key}) : super(key: key);
+
+  @override
+  State<FirstPage> createState() => _FirstPageState();
+}
+
+class _FirstPageState extends State<FirstPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AppBar(
+          title: Text('Agri Helper'), backgroundColor: Colors.lightGreen),
+    );
+  }
+}
